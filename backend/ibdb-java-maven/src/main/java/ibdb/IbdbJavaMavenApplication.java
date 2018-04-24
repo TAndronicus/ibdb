c7441bc16961787cbaa2fb166063d65038c1d5c0
Jędrzej Biedrzycki <jedrzejjanbiedrzycki@gmail.com>
2018-04-24 20:39:13 +0200

    Remove runtime postgres not implemented error

--- backend/ibdb-java-maven/src/main/java/ibdb/IbdbJavaMavenApplication.java
+++ backend/ibdb-java-maven/src/main/java/ibdb/IbdbJavaMavenApplication.java
@@ -1,10 +1,7 @@
 package ibdb;
 
-import ibdb.model.repo.UserRepo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories
